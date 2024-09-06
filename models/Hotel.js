f5466bbfc31d70ba418cdaed84bed46d44a11776
GADEKAR021 <161955509+GADEKAR021@users.noreply.github.com>
2024-09-06 23:19:08 +0530

    comiit

--- models/Hotel.js
+++ models/Hotel.js
@@ -1,61 +1,60 @@
-const mongoose = require("mongoose")
-
+const mongoose = require("mongoose");
 
 const HotelSchema = new mongoose.Schema(
-    {
-        name: {
-            type: String,
-            required: true,
-        },
-        type : {
-            type: String,
-            required: true,
-        },
-        country: {
-            type: String,
-            required: true
-        },
-        address: {
-            type: String,
-            required: true
-        },
-        city: {
-            type: String
-        },
-        distance: {
-            type: String
-        },
-        photos: {
-            type: [String]
-        },
-        title : {
-            type: String,
-            required: true
-        },
-        desc : {
-            type: String,
-            required: false
-        },
-        rating : {
-            type: Number,
-            min: 0,
-            max: 5,
-        },
-        rooms : {
-            type: [String],
-        },
-        cheapestPrice : {
-            type: Number,
-            required : true
-        },
-        featured : {
-            type: Boolean,
-            default: false
-        },
+  {
+    name: {
+      type: String,
+      required: true,
     },
-    {
-        timestamps: true
-    }
-)
+    type: {
+      type: String,
+      required: true,
+    },
+    country: {
+      type: String,
+      required: true,
+    },
+    address: {
+      type: String,
+      required: true,
+    },
+    city: {
+      type: String,
+    },
+    distance: {
+      type: String,
+    },
+    photos: {
+      type: [String],
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    desc: {
+      type: String,
+      required: false,
+    },
+    rating: {
+      type: Number,
+      min: 0,
+      max: 5,
+    },
+    rooms: {
+      type: [String],
+    },
+    cheapestPrice: {
+      type: Number,
+      required: true,
+    },
+    featured: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 
-module.exports = mongoose.model('Hotel', HotelSchema)
+module.exports = mongoose.model("Hotel", HotelSchema);
